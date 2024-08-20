f3afb8cf8cb618ec705ff6986fea4f5d76da5cfe
Ali Pourgheysari <a.geysar@gmail.com>
2024-08-20 14:01:37 +0330

    Add a combination of number and scale

--- Extract_data_for_Easyocr.py
+++ Extract_data_for_Easyocr.py
@@ -64,7 +64,6 @@ for i in range(0, len(data)):
     bboxes = data[i]['bbox']
     transcriptions = data[i]['transcription']
     filename = re.split('/ |-', data[i]['ocr'])[-1].replace('.jpg', '.txt')
-    title = ""
 
     for j in range(0, len(bboxes)):
         original_width = bboxes[j]['original_width']
@@ -77,24 +76,45 @@ for i in range(0, len(data)):
 
         transcript = transcriptions[j]
         
-        for k, label in enumerate(data[i]['label']):
-            if  title != "":
-                break
-            if 'Title' in label['labels']:
-                title = transcriptions[k]
-                x1_title, y1_title = int(label['x'] * original_width / 100), int(label['y'] * original_height / 100)
-                x3_title, y3_title = int((bboxes[k]['x'] + bboxes[k]['width']) * original_width / 100), int((bboxes[k]['y'] + bboxes[k]['height']) * original_height / 100)
-                break
-        
-        if title != "":
-            if x1 > x1_title and y1 > y1_title and x3 < x3_title and y3 < y3_title:
-                continue                
-        
+        # create combined bbox for OCR including scale + number
+        combined_transcript = ''
+        combined_x1, combined_y1 = '', ''
+        if len(transcriptions) <= 18:
+            for k, label in enumerate(data[i]['label']):
+                if k == j and 'Number' in label['labels']:
+                    for l, label2 in enumerate(data[i]['label']):
+                        if 'Scale' in label2['labels']:
+                            scale_y1 = int(label2['y'] * original_height / 100)
+                            scale_y3 = int((label2['y'] + label2['height']) * original_height / 100)
+                            mid = (y1 + y3) / 2
+
+                            if mid > scale_y1 and mid < scale_y3:
+                                scale_x1 = int(label2['x'] * original_width / 100)
+                                scale_x3 = int((label2['x'] + label2['width']) * original_width / 100)
+                                # generalize bbox 
+                                if x1 > scale_x1:
+                                    combined_x1 = scale_x1
+                                    combined_transcript = transcriptions[l] + ' ' + transcript
+                                else:
+                                    combined_x1 = x1
+                                    combined_transcript = transcript + ' ' + transcriptions[l]
+
+                                if x3 < scale_x3:
+                                    combined_x3 = scale_x3
+                                    combined_transcript = transcript + ' ' + transcriptions[l]
+                                else:
+                                    combined_x3 = x3
+                                    combined_transcript = transcriptions[l] + ' ' + transcript
+                                combined_y1 = min(y1, scale_y1)
+                                combined_y3 = max(y3, scale_y3)
+
         Craft_list.append(f'{x1},{y1},{x2},{y2},{x3},{y3},{x4},{y4},{transcript}\n')
         
         # Crop the image for Ocr
         img = cv2.imread('./Data/Cropped/' + re.split('-', data[i]['ocr'])[-1])
         crop_img = img[y1:y3, x1:x3]
+        if combined_x1 != '':
+            crop_comb_img = img[combined_y1:combined_y3, combined_x1:combined_x3]
         cropped_filename = filename.replace('.txt', f'_{j}.jpg')
 
         if random.random() < train_split:
@@ -105,7 +125,11 @@ for i in range(0, len(data)):
         with open(img_path + 'labels.csv', mode='a', newline='', encoding="utf8") as file:
             writer = csv.writer(file)
             writer.writerow([cropped_filename, transcript])
+            if combined_x1 != '':
+                writer.writerow([cropped_filename.replace('.jpg', '_comb.jpg'), combined_transcript])
             cv2.imwrite(img_path + f'{cropped_filename}', crop_img)
+            if combined_x1 != '':
+                cv2.imwrite(img_path + f'{cropped_filename.replace(".jpg", "_comb.jpg")}', crop_comb_img)
     
     # save data for CRAFT
     txt = 'gt_' + filename
